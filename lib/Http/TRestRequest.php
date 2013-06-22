00367e47781765df50bef0d4197570fffc1dce7f
marcosmercedesn <marcos.mercedesn@gmail.com>
2013-06-22 03:01:46 -0400

    Documenting TRestRequest

--- lib/Http/TRestRequest.php
+++ lib/Http/TRestRequest.php
@@ -4,18 +4,32 @@
  * Class that handles the abstraction of a HTTP Request to a REST web service.
  *
  * @author Marcos Mercedes <marcos.mercedesn@gmail.com>
+ * @package TRest\Http
  */
 namespace TRest\Http;
 
 class TRestRequest extends TRestRequestProperties {
 
+    /**
+     * A hashed url used as an unique key in order to be able to use caching for
+     * the responses
+     *
+     * @return string
+     */
     public function getUrlHash() {
         return md5($this->buildUrl() . '?' . implode('&', $this->getParameters()));
     }
 
+    /**
+     *
+     * @param boolean $addParameters
+     *            decides if the url parameters should be added to the url
+     *            returned
+     * @return string
+     */
     public function buildUrl($addParameters = true) {
         $array = array(
-            rtrim($this->getUrl(),'/'),
+            rtrim($this->getUrl(), '/'),
             $this->getResource()
         );
         if ($this->getPath())
